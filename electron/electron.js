27dbf5337e49e9e498395546efc32d9e5c816659
srlsrx <nicolasin97@gmail.com>
2025-06-25 00:42:15 +0200

    first commit

---
+++ electron/electron.js
@@ -0,0 +1,40 @@
+// electron.js (versión ESM)
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import isDev from 'electron-is-dev';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = isDev
+    ? 'http://localhost:5173'
+    : `file://${path.join(__dirname, 'dist/index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow();
+});
